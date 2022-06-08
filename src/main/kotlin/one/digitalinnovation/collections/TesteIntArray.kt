a13fb5265dd40c23280cfc64c5d2000f717fb5ae
lucas.pastre <lucasfpastre@gmail.com>
2022-06-08 15:15:37 -0300

    Criado uma classe de teste de um array de int

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -0,0 +1,24 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val values = IntArray(5)
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values) {
+        println(valor)
+    }
+    values.sortDescending()
+    println("-----------------------------")
+    values.forEach {valor ->
+        println(valor)
+    }
+    values.sort()
+    println("-----------------------------")
+    for (index in values.indices) {
+        println(values[index])
+    }
+}
