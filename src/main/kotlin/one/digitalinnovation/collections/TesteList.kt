e5876b3f6fb1d223f487863479500fd12a977732
lucas.pastre <lucasfpastre@gmail.com>
2022-06-08 16:50:15 -0300

    Testes com listas

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,24 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("Joao", 1000.0)
+    val pedro = Funcionario("Pedro", 2000.0)
+    val maria = Funcionario("Maria", 4000.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it) }
+
+    println(funcionarios.find {it.nome == "Maria"})
+}
+
+data class Funcionario (
+    val nome: String,
+    val salario: Double
+) {
+    override fun toString(): String = """
+        Nome:    $nome
+        Salario: $salario
+        ------------------------
+    """.trimIndent()
+}
